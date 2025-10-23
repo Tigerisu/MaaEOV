5608185f5a9cd6372f0e1b2a45f558f0107d8843
Tigerisu <122464353+Tigerisu@users.noreply.github.com>
2025-10-23 11:01:20 +0800

    Adopt install.yml and dependencies from M9A. Modify license to match M9A.

--- tools/ci/install.py
+++ tools/ci/install.py
@@ -4,26 +4,28 @@ import shutil
 import sys
 import json
 
+import os
+import sys
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(script_dir)
+
 from configure import configure_ocr_model
 
-
-working_dir = Path(__file__).parent.parent
+working_dir = Path(__file__).parent.parent.parent
 install_path = working_dir / Path("install")
 version = len(sys.argv) > 1 and sys.argv[1] or "v0.0.1"
 
 
 def install_deps():
-    if not (working_dir / "deps" / "bin").exists():
-        print("Please download the MaaFramework to \"deps\" first.")
-        print("请先下载 MaaFramework 到 \"deps\"。")
-        sys.exit(1)
-
     shutil.copytree(
         working_dir / "deps" / "bin",
         install_path,
         ignore=shutil.ignore_patterns(
             "*MaaDbgControlUnit*",
             "*MaaThriftControlUnit*",
+            "*MaaWin32ControlUnit*",
             "*MaaRpc*",
             "*MaaHttp*",
         ),
@@ -38,7 +40,7 @@ def install_deps():
 
 def install_resource():
 
-    # configure_ocr_model()
+    configure_ocr_model()
 
     shutil.copytree(
         working_dir / "assets" / "resource",
@@ -54,20 +56,25 @@ def install_resource():
         interface = json.load(f)
 
     interface["version"] = version
+    interface["custom_title"] = f"MaaEOV {version} | 异象回声小助手"
 
     with open(install_path / "interface.json", "w", encoding="utf-8") as f:
         json.dump(interface, f, ensure_ascii=False, indent=4)
 
 
 def install_chores():
-    shutil.copy2(
-        working_dir / "README.md",
-        install_path,
+    for file in ["README.md", "LICENSE", "requirements.txt"]:
+        shutil.copy2(
+            working_dir / file,
+            install_path,
+        )
+    shutil.copytree(
+        working_dir / "docs",
+        install_path / "docs",
+        dirs_exist_ok=True,
+        ignore=shutil.ignore_patterns("*.yaml"),
     )
-    shutil.copy2(
-        working_dir / "LICENSE",
-        install_path,
-    )
+
 
 def install_agent():
     shutil.copytree(
@@ -75,6 +82,22 @@ def install_agent():
         install_path / "agent",
         dirs_exist_ok=True,
     )
+
+    with open(install_path / "interface.json", "r", encoding="utf-8") as f:
+        interface = json.load(f)
+
+    if sys.platform.startswith("win"):
+        interface["agent"]["child_exec"] = r"{PROJECT_DIR}/python/python.exe"
+    elif sys.platform.startswith("darwin"):
+        interface["agent"]["child_exec"] = r"{PROJECT_DIR}/python/bin/python3"
+    elif sys.platform.startswith("linux"):
+        interface["agent"]["child_exec"] = r"python3"
+
+    interface["agent"]["child_args"] = ["-u", r"{PROJECT_DIR}/agent/main.py"]
+
+    with open(install_path / "interface.json", "w", encoding="utf-8") as f:
+        json.dump(interface, f, ensure_ascii=False, indent=4)
+
 
 if __name__ == "__main__":
     install_deps()
